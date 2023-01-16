f89a5efda0aacfbd479a977c6ea2bf5ffacfefcb
yunji <yunjigo92@gmail.com>
2023-01-16 13:23:23 +0900

    feat : thread unsafe 실습
    - 하나의 값을 여러 스레드에서 접근할 때 해당 상태값을 저장해서 사용하는 경우 스레드 세이프 하지 않기 때문에 예상과 다른 결과가 도출될 수 있으므로, 이렇게 사용하지 않도록 주의한다.

---
+++ src/main/java/org/example/counter/Counter.java
@@ -0,0 +1,34 @@
+package org.example.counter;
+
+/**
+ * 스레드는 특정 상태를 저장하게 해서는 안된다.
+ * 스레드 safe 하지 않고 생각과 다른 결과가 나올 수 있다.
+ *
+ * @author : yunji
+ * @version : 1.0.0
+ * @date : 2023/01/16
+ */
+public class Counter implements Runnable{
+
+    private int count = 0;
+
+    public void increment(){
+        count++;
+    }
+
+    public void decrement(){
+        count--;
+    }
+
+    public int getValue(){
+        return count;
+    }
+
+    @Override
+    public void run() {
+        this.increment();
+        System.out.println("value for thread after increment" + Thread.currentThread().getName() + " " + this.getValue());
+        this.decrement();
+        System.out.println("value for thread at last" + Thread.currentThread().getName() + " " + this.getValue());
+    }
+}
---
+++ src/main/java/org/example/counter/RaceConditionDemo.java
@@ -0,0 +1,22 @@
+package org.example.counter;
+
+/**
+ * 하나의 객체를 다중 스레드에서 접근할 경우
+ *
+ * @author : yunji
+ * @version : 1.0.0
+ * @date : 2023/01/16
+ */
+public class RaceConditionDemo {
+
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        Thread t1 = new Thread(counter, "Thread-1");
+        Thread t2 = new Thread(counter, "Thread-2");
+        Thread t3 = new Thread(counter, "Thread-3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
